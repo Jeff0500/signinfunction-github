121b70b4788c9cd0c5a535670cb357e211cdaabb
Jeff <duck1314521@gmail.com>
2024-12-27 23:05:43 +0800

    顯示資訊頁面套用css

--- showinfo.php
+++ showinfo.php
@@ -17,14 +17,6 @@ require 'vendor/autoload.php';
     $email = $_SESSION['email'];
     $birthday = $_SESSION['birthday'];
     $created_at = date('Y-m-d H:i:s');  // 當前時間
-    echo "<pre>";
-    echo "First Name: " . $first_name . "<br>";
-    echo "Last Name: " . $last_name . "<br>";
-    echo "Phone: " . $phone . "<br>";
-    echo "Email: " . $email . "<br>";
-    echo "Birthday: " . $birthday . "<br>";
-    echo "Created At: " . $created_at . "<br>";
-    echo "</pre>";
 
 
 //檢查是否有資料
@@ -57,7 +49,7 @@ if (isset($_SESSION['first_name'])) {
 
     // 執行查詢並檢查結果
     if ($stmt->execute()) {
-        echo "<p>資料已成功插入資料庫！</p>";
+        echo "";
     } else {
         echo "<p>錯誤: " . $stmt->error . "</p>";
     }
@@ -70,3 +62,34 @@ if (isset($_SESSION['first_name'])) {
     echo "無法獲取資料，請返回重新填寫資料。";
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="zh-tw">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>資料提交成功</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="background"></div>
+
+    <!-- 顯示資料處理的容器 -->
+    <div class="form-container">
+        <h1>資料提交結果</h1>
+
+
+        <p><strong>資料預覽:</strong></p>
+        <ul>
+            <li><p class="p2">您的姓名: <?php echo htmlspecialchars($first_name." ".$last_name); ?></p></li>
+            <li><p class="p2">您的手機: <?php echo htmlspecialchars($phone); ?></p></li>
+            <li><p class="p2">您的信箱: <?php echo htmlspecialchars($email); ?></p></li>
+            <li><p class="p2">您的生日: <?php echo htmlspecialchars($birthday); ?></p></li>
+            <li><p class="p2">資料建立時間: <?php echo htmlspecialchars($created_at); ?></p></li>
+        </ul>
+        <p><strong>資料提交成功，謝謝您成為本購物平台會員!</strong></p>
+
+        <a href="index.html"><button>返回首頁</button></a>
+    </div>
+</body>
+</html>
